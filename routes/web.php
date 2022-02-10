185b93e7c58c1f88277d1de4b4524e82e68490a3
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-10 18:04:11 -0300

    Fallback

--- routes/web.php
+++ routes/web.php
@@ -38,10 +38,12 @@ Route::get('/rota1', function(){
     echo 'Rota1';
 })->name('site.rota1');
 
-
 Route::get('/rota2', function(){
     return redirect()->route('site.rota1');
 })->name('site.rota2');
 
-
 //Route::redirect('/rota2', '/rota1');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para página inicial.';
+});
