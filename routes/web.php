9003e2b5e1cbe68e01c648bebecb37a8b19b5332
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-10 11:15:23 -0300

    Parametros de rotas com expressões regulares

--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,11 @@ Route::get('/contato', [ContatoController::class, 'contato']);
 
 // nome, categoria, assunto, mensagem
 Route::get(
-    '/contato/{nome?}/{categoria?}/{asunto?}/{mensagem?}',
+    '/contato/{nome}/{categoria_id}',
     function(
         string $nome = 'Desconhecido',
-        string $categoria = 'Informação',
-        string $assunto = 'Contato',
-        string $mensagem = 'mensagem não informada'
+       int $categoria_id = 1 // 1 - 'Informação'
     ){
-        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+        echo "Estamos aqui: $nome - $categoria_id";
     }
-);
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
