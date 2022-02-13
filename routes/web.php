aa52ff212a10deffa96df3540e54a50c3073a0da
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-13 17:59:53 -0300

    Encaminhando parametros da rota para o controlador

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,15 +35,7 @@ Route::prefix('/app')->group(function(){
 });
 
 
-Route::get('/rota1', function(){
-    echo 'Rota1';
-})->name('site.rota1');
-
-Route::get('/rota2', function(){
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-//Route::redirect('/rota2', '/rota1');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
 
 Route::fallback(function(){
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para página inicial.';
