6874c0f9929b89bbd7776c1fe4ef8907d34828d2
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-14 14:35:33 -0300

    Blade - Comentários

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,18 @@
+<h3>Fornecedor</h3>
+{{-- Fica o comanetário que será descartado pelo interpretador do blade --}}
+{{'Texto de teste'}}
+<?= 'Texto de teste' ?>
+
+@php
+
+    //Para comanetário de uma linha
+
+    /*
+        Para
+        comentário
+        de multiplas
+        linhas
+    */
+
+    echo 'Texto de teste'
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\TesteController;
+use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,7 +31,7 @@ Route::get('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
     Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
 
