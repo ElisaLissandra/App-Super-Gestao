88b283ae9839ed65204f5bef740e12178968039d
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-14 11:51:21 -0300

    Encaminhando parametros do controlador para visualização

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $p1, int $p2){
-        echo "A soma de $p1 + $p2 é: ".($p1 +$p2);
+        //echo "A soma de $p1 + $p2 é: ".($p1 +$p2);
+        //return view('site.teste', ['x' => $p1, 'y' => $p2]); //array associativo
+
+        return view('site.teste', compact('p1', 'p2')); //compact
+
+        return view('site.teste')->with('p1', $p1)->with('p1', $p2); //with()
     }
 }
