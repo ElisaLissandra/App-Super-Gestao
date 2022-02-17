a782a708b445890c50a2c40da822c878fe5d4950
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-17 11:08:22 -0300

    Implementação do asset

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,31 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '11', //São Paulo (SP)
+                'telefone' => '0000-0000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => 'null',
+                'ddd' => '85', //Fortaleza (CE)
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => 'null',
+                'ddd' => '32', //Juiz de Fora (MG)
+                'telefone' => '0000-0000'
+            ]
+        ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
+
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,18 +1,43 @@
 <h3>Fornecedor</h3>
-{{-- Fica o comanetário que será descartado pelo interpretador do blade --}}
-{{'Texto de teste'}}
-<?= 'Texto de teste' ?>
 
 @php
+/*
+    if(){
 
-    //Para comanetário de uma linha
+    } elseif() {
 
-    /*
-        Para
-        comentário
-        de multiplas
-        linhas
+    } else {
+
+    }
     */
-
-    echo 'Texto de teste'
 @endphp
+
+@isset($fornecedores)
+    
+    @forelse($fornecedores as $indice => $fornecedor)
+        Iteração atual: {{ $loop->iteration }}
+        <br />
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br />
+        Status: {{ $fornecedor['status'] }}
+        <br />
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não informado'}}
+        <br />
+        Telefone: ({{ $fornecedor['ddd'] ?? ''}}) {{ $fornecedor['telefone'] ?? ''}}
+        <br />
+        @if($loop->first)
+            Primeira iteração do loop
+        @endif
+        @if($loop->last)
+           Última iteração do loop
+
+           <br />
+           Total de registros: {{ $loop->count}}
+        @endif
+       <hr />
+    @empty
+        Não existe fornacedores cadastrados! 
+    @endforelse
+@endisset
+
+
