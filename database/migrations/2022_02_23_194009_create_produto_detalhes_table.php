02e9f1a5bce9ffd3d14eb8fef321449cae682a5b
ElisaLissandra <elisa_lissandra@outlook.com>
2022-02-23 17:19:29 -0300

    Migration - Relacionamento um para um

---
+++ database/migrations/2022_02_23_194009_create_produto_detalhes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
+            //colunas
+            $table->id();
+            //chave estrangeira
+            $table->unsignedBigInteger('produto_id');
+            $table->float('comprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+            $table->timestamps();
+
+            //constraint
+            $table->foreign('produto_id')->references('id')->on('produtos'); // Foreign Key
+            $table->unique('produto_id');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_detalhes');
+    }
+}
