b5a72c8c97e20a5b5605a3a42ac2ea9b5b47c7d1
ElisaLissandra <elisa_lissandra@outlook.com>
2022-03-03 19:43:44 -0300

    Validação de quantidade

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -41,11 +41,11 @@ class ContatoController extends Controller
     public function salvar(Request $request) {
      //realizar a validação dos dados do formulário recebidos no  request
      $request->validate([
-        'nome' => 'required',
+        'nome' => 'required|min:3|max:40',
         'telefone'=> 'required',
         'email' => 'required',
         'motivo_contato' => 'required',
-        'mensagem' => 'required'
+        'mensagem' => 'required|max:2000'
 
      ]);
 
