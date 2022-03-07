092fdda4327869e5f25d7c621f7ebd7417515ef0
ElisaLissandra <elisa_lissandra@outlook.com>
2022-03-07 16:55:24 -0300

    Request old input

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,33 +9,14 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        /*
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
-        */
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
 
-        /*
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
 
-        //print_r($contato->getAttributes());
-        $contato->save();
-        */
-
-        //$contato = new SiteContato();
-        //$contato->create($request->all());
-        //$contato->save();
-        //print_r($contato->getAttributes());
-
-        return view('site.contato', ['titulo' => 'Contato']);
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
