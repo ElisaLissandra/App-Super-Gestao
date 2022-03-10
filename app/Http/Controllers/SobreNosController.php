bf7da123550852f799b49a3751abf3b3f7d0c9d4
ElisaLissandra <elisa_lissandra@outlook.com>
2022-03-10 17:00:40 -0300

    Apelidando middleware

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
+
+   public function __construct() {
+      $this->middleware('log.acesso');
+   }
+
    public function sobreNos() {
       return view('site.sobre-nos', ['titulo' => 'Sobre Nós']);
    }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index')->middleware('log.acesso');
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
