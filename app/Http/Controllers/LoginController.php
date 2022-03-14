ed31e62773b4843df60c12f0392cc93209c59b14
ElisaLissandra <elisa_lissandra@outlook.com>
2022-03-14 16:37:29 -0300

    Recebimento de parâmetro

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,22 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar() {
-        return 'Chegamos até aqui!';
+    public function autenticar(Request $request) {
+
+        //regras de validação
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        //mensagens de feedback  de validação
+        $feedback = [
+            'usuario.email' => 'O  campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        print_r($request->all());
     }
 }
