86831c79d65089c86409ffbe5ecf8fd094fea8bb
ElisaLissandra <elisa_lissandra@outlook.com>
2022-03-16 15:31:03 -0300

    Implementação de menu da área protegida

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,31 +7,7 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        $fornecedores = [
-            0 => [
-                'nome' => 'Fornecedor 1',
-                'status' => 'N',
-                'cnpj' => '0',
-                'ddd' => '11', //São Paulo (SP)
-                'telefone' => '0000-0000'
-            ],
-            1 => [
-                'nome' => 'Fornecedor 2',
-                'status' => 'S',
-                'cnpj' => 'null',
-                'ddd' => '85', //Fortaleza (CE)
-                'telefone' => '0000-0000'
-            ],
-            2 => [
-                'nome' => 'Fornecedor 3',
-                'status' => 'S',
-                'cnpj' => 'null',
-                'ddd' => '32', //Juiz de Fora (MG)
-                'telefone' => '0000-0000'
-            ]
-        ];
 
-        return view('app.fornecedor.index', compact('fornecedores'));
-
+        return view('app.fornecedor');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\TesteController;
 use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -36,9 +39,11 @@ Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.logi
 Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:padrao, visitante, p3, p4')->prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+    Route::get('/home', [HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair',[LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
+    Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
 
 
