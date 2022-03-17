d7ecd7371167105188c49b0953e39be48382f70a
ElisaLissandra <elisa_lissandra@outlook.com>
2022-03-16 21:19:14 -0300

    Implementação do cadastro Fornecedores parte-1

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,7 +7,14 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
+        return view('app.fornecedor.index');
+    }
 
-        return view('app.fornecedor');
+    public function listar() {
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar() {
+        return view('app.fornecedor.adicionar');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,11 @@ Route::middleware('autenticacao:padrao, visitante, p3, p4')->prefix('/app')->gro
     Route::get('/home', [HomeController::class, 'index'])->name('app.home');
     Route::get('/sair',[LoginController::class, 'sair'])->name('app.sair');
     Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
+
     Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
+    Route::GET('/fornecedor/adicionar', [FornecedorController::class, 'adicionar'])->name('app.fornecedor.adicionar');
+
     Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
 
